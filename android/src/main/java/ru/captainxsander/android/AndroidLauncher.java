e6e7523e9a54491fde1558bd35f0345f29d12a91
Alexander Kulikov <Kulikov.A.Aleksandro@sberbank.ru>
2026-04-04 21:12:59 +0300

    Initial commit

---
+++ android/src/main/java/ru/captainxsander/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package ru.captainxsander.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.captainxsander.DesktopLauncher;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new DesktopLauncher(), configuration);
+    }
+}
