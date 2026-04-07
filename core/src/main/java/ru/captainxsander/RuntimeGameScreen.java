58bb379b466a166afaa90ec3b4931343c75d8937
Alexander K. <kulikv.aleksanndr@gmail.com>
2026-04-07 23:32:16 +0300

    Add runtime game screen

---
+++ core/src/main/java/ru/captainxsander/RuntimeGameScreen.java
@@ -0,0 +1,84 @@
+package ru.captainxsander;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RuntimeGameScreen implements Screen {
+
+    private World world;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private SpriteBatch batch;
+
+    private Floor floor;
+    private WinZone winZone;
+    private Claw claw;
+
+    private final List<Toy> toys = new ArrayList<>();
+
+    @Override
+    public void show() {
+        Box2D.init();
+        world = new World(new Vector2(0, -9.8f), true);
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(16f, 9f, camera);
+        viewport.apply(true);
+
+        batch = new SpriteBatch();
+
+        floor = new Floor();
+        floor.create(world);
+
+        winZone = new WinZone();
+        winZone.create(world);
+
+        claw = new Claw();
+        claw.create();
+
+        toys.add(new Toy(world, 6f, 1.1f, "pig.png"));
+        toys.add(new Toy(world, 7f, 1.1f, "cow.png"));
+    }
+
+    @Override
+    public void render(float delta) {
+        RuntimeTuning.updateInput();
+
+        world.step(1/60f, 6, 2);
+
+        ScreenUtils.clear(0,0,0,1);
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+
+        floor.render(batch);
+        winZone.render(batch);
+
+        for (Toy toy : toys) toy.render(batch);
+
+        claw.render(batch);
+
+        batch.end();
+    }
+
+    @Override public void resize(int width, int height) { viewport.update(width, height, true); }
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        world.dispose();
+    }
+}
