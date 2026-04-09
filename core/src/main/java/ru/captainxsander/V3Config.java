c587706bf90ba6fdb9a076c5319322ba503731cc
Alexander K. <kulikv.aleksanndr@gmail.com>
2026-04-09 23:09:38 +0300

    Add v3 physics claw config

---
+++ core/src/main/java/ru/captainxsander/V3Config.java
@@ -0,0 +1,23 @@
+package ru.captainxsander;
+
+public final class V3Config {
+    private V3Config() {}
+
+    public static final float WORLD_WIDTH = 16f;
+    public static final float WORLD_HEIGHT = 9f;
+
+    public static final float HOME_X = 8f;
+    public static final float CARRIAGE_Y = 8.35f;
+    public static final float HOME_CABLE = 1.05f;
+    public static final float MAX_CABLE = 5.55f;
+
+    public static final float CARRIAGE_SPEED = 3.6f;
+    public static final float CABLE_SPEED = 2.6f;
+
+    public static final float TRAY_X = 14.1f;
+    public static final float TRAY_Y = 0.45f;
+    public static final float TRAY_W = 2.25f;
+    public static final float TRAY_H = 2.15f;
+
+    public static final float TOY_RADIUS = 0.38f;
+}
