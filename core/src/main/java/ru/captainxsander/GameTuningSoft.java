d941265e2ac9acb3a1e0ffc8de9fc28a6ff26b06
Alexander Kulikov <Kulikov.A.Aleksandro@sberbank.ru>
2026-04-10 20:37:50 +0300

    revert

--- core/src/main/java/ru/captainxsander/GameTuningSoft.java
+++
@@ -1,7 +0,0 @@
-package ru.captainxsander;
-
-public class GameTuningSoft extends GameTuning {
-    public static final float TOY_TRAY_GRAVITY_SCALE = 0.18f;
-    public static final float TOY_LINEAR_DAMPING = 1.2f;
-    public static final float SWING_DAMPING = 0.7f;
-}
